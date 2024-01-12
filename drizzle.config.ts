440d7ef59a38fa21b10d1271a25570badaede669
jungle379 <jungle379@gmail.com>
2024-01-13 08:13:20 +0900

    a

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "drizzle-kit";
-
-const drizzleConfig = {
-  schema: "./src/db/schema.ts",
-  out: "./src/db/migrations",
-  breakpoints: true,
-} satisfies Config;
-
-export default drizzleConfig;
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export const runtime = "edge";
-
-export default function Home() {
-  return (
-    <main>
-      <h1>テストページ</h1>
-    </main>
-  );
-}
