9667c4c1a9a4c55d42255581ebc3b2e2a23f685b
jungle379 <jungle379@gmail.com>
2024-01-13 10:41:41 +0900

    modify page.tsx to display database contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
+import { db } from "@/db";
+
 export const runtime = "edge";
 
-export default function Home() {
+export default async function Home() {
+  const users = await db.query.users.findMany({
+    with: {
+      posts: true,
+    },
+  });
+
   return (
-    <main>
-      <h1>テストページ</h1>
-    </main>
+    <>
+      <div className="grid grid-cols-2 gap-x-5 gap-y-5 my-5 mx-auto max-w-2xl">
+        {(await users).map((user, index) => (
+          <div key={user.id} className="bg-blue-100 rounded-md p-5">
+            <h1 className="text-lg font-bold">{user.name}</h1>
+            <ul>
+              <li>{user.email}</li>
+            </ul>
+            <div className="bg-red-100 mt-5 p-5 rounded-md">
+              <h1 className="text-sm font-bold">記事投稿</h1>
+              <ul>
+                <li>
+                  {user.posts.map((post) => (
+                    <div key={post.id}>
+                      <h1 className="text-sm font-bold mt-5">{post.title}</h1>
+                      <ul>
+                        <li>{post.content}</li>
+                      </ul>
+                    </div>
+                  ))}
+                </li>
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
