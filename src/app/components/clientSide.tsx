174f01b1d461347d172ef908bfb750dab1e6e184
jungle379 <jungle379@gmail.com>
2024-01-30 23:03:10 +0900

    client

--- src/app/components/clientSide.tsx
+++ src/app/components/clientSide.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export const ClientComponent = () => {
+  const [test, setTest] = useState();
+  const test1 = () => {
+    console.log("OK!");
+  };
+  useEffect(() => {
+    if (!test1) return;
+    console.log("msg");
+  }, [test]);
   return (
-    <>
-      <div>aaa</div>
-    </>
+    <div className="p-8">
+      <h1>トップページ</h1>
+      <button onClick={test1}>テスト</button>
+    </div>
   );
 };
